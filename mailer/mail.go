18a2c9f44d98e3f42d282d680995f6cd60bdb22f
fixNpatch <fix@patch.com>
2020-10-23 12:47:09 +0400

    разбираюсь с правильностью ввода лайнов

--- mailer/mail.go
+++ mailer/mail.go
@@ -11,42 +11,42 @@ import (
 const Inbox = "mail/inbox"
 
 type Mail_ struct {
-	Address Address
+	Address        Address
 	NominalAddress NominalAddress
-	Headers Headers
-	Body Body
+	Headers        Headers
+	Body           Body
 }
 
 type Address struct {
 	From string
-	To string
+	To   string
 }
 
 type NominalAddress struct {
 	From string
-	TO string
+	TO   string
 }
 
 type Headers struct {
-	Date string
+	Date      string
 	MessageId string
-	Subject string
+	Subject   string
 
 	MIME string
 
 	AuthenticationResults string
-	ReceivedSPF string
+	ReceivedSPF           string
 
 	XHeaders
 }
 
 type XHeaders struct {
 	XConfirmReadingTo string
-	XErrorsTo string
-	XMailer string
-	XSender string
-	XPriority string
-	XUIDL string
+	XErrorsTo         string
+	XMailer           string
+	XSender           string
+	XPriority         string
+	XUIDL             string
 }
 
 type Body struct {
@@ -62,48 +62,91 @@ func readfile(filename string) string {
 	return string(data)
 }
 
-func (m *Mail_) Parser(info os.FileInfo)(error)  {
+func (m *Mail_) Parser(info os.FileInfo) error {
 	readed := readfile(info.Name())
 	if readed == "" {
 		fmt.Println("Error::Cant read file")
 		return fmt.Errorf("Error:Cant read file::Empty input")
 	}
 
+	// formating
 
-	for _, line := range strings.Split(strings.TrimSuffix(readed, "\n"), "\n") {
+	//regexp для заголовков ^[a-zA-Z].[a-zA-Z-0-9]+:
 
-		if len(line) >= 15 && line[:12] == "Delivered-To" {
-			m.NominalAddress.TO = line[14:]
+	rawLineArray := strings.Split(strings.TrimSuffix(readed, "\n"), "\n")
+	lastHeaderIndex := 0
+	var modifiedLineArray []string
+
+	//match := false
+
+	fmt.Println("===============================================\n===============================================\n===============================================\n")
+	for _, line := range rawLineArray {
+		fmt.Println(line + "\n")
+	}
+	fmt.Println("===============================================\n===============================================\n===============================================\n")
+
+	for index, line := range rawLineArray {
+
+		//
+		//if index == 0 {
+		//	modifiedLineArray = append(modifiedLineArray, line)
+		//}
+
+		// если lineArray[index] содержит заголовок (проверяем при помощи regexp)
+		// то добавляем эту строку к последнему заголовку
+
+		match, err := regexp.MatchString("^[A-Z].[a-zA-Z-0-9]+:", line)
+		if err != nil {
+			fmt.Println("Error::Something wrong with your Regexp")
+			return fmt.Errorf("Error:Cant parse file::Bad input")
 		}
 
-		if len(line) >= 14 && line[:11] == "Return-path" {
-			m.NominalAddress.From = line[13:]
+		if !match {
+			if index > len(rawLineArray) {
+				modifiedLineArray[lastHeaderIndex] += " " + line
+			}
+		} else {
+			lastHeaderIndex = index
+			modifiedLineArray = append(modifiedLineArray, line)
 		}
 
-		if len(line) >= 25 && line[:22] == "Authentication-Results" {
-			m.Address.From = line[24:]
-		}
+		//if line
 
-
-		if len(line) >= 7 && line[:4] == "From" {
-			m.Address.From = line[6:]
-		}
-
-		if len(line) >= 5 && line[:2] == "From" {
-			m.Address.From = line[4:]
-		}
-
-		if len(line) >= 12 && line[:10] == "Message-ID" {
-			m.Headers.MessageId = line[11:]
-		}
-
-		if len(line) >=9 && line[:7] == "Subject" {
-			m.Headers.Subject = line[8:]
-		}
-
+		//if len(line) >= 15 && line[:12] == "Delivered-To" {
+		//	m.NominalAddress.TO = line[14:]
+		//}
+		//
+		//if len(line) >= 14 && line[:11] == "Return-path" {
+		//	m.NominalAddress.From = line[13:]
+		//}
+		//
+		//if len(line) >= 25 && line[:22] == "Authentication-Results" {
+		//	m.Address.From = line[24:]
+		//}
+		//
+		//
+		//if len(line) >= 7 && line[:4] == "From" {
+		//	m.Address.From = line[6:]
+		//}
+		//
+		//if len(line) >= 5 && line[:2] == "From" {
+		//	m.Address.From = line[4:]
+		//}
+		//
+		//if len(line) >= 12 && line[:10] == "Message-ID" {
+		//	m.Headers.MessageId = line[11:]
+		//}
+		//
+		//if len(line) >=9 && line[:7] == "Subject" {
+		//	m.Headers.Subject = line[8:]
+		//}
 
 	}
 
+	// для проверки выведем. потом можно удалить этот кусок кода
+	for _, line := range modifiedLineArray {
+		fmt.Println(line)
+	}
 
 	//fmt.Println(readed)
 
@@ -114,7 +157,7 @@ func (m *Mail_) Parser(info os.FileInfo)(error)  {
 	return nil
 }
 
-func (m *Mail_) Checker()  {
+func (m *Mail_) Checker() {
 
 	fmt.Println(m.NominalAddress.TO)
 	fmt.Println(m.NominalAddress.From)
@@ -123,8 +166,7 @@ func (m *Mail_) Checker()  {
 	//reasonsNumber=0
 
 	//if len(m.Headers.Subject) > 120:
-		//reasons
-
+	//reasons
 
 	return
 }
